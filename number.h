b81ac42c061868360d6795a7b36b3a66e57d2f42
鰤谷光太 <211063@st.yoshida-g.ac.jp>
2022-09-30 00:56:39 +0900

    空白の修正

--- number.h
+++ number.h
@@ -4,25 +4,22 @@
 // Author:hamada ryuuga
 //
 //============================
-
 #ifndef _NUMBER_H_
 #define _NUMBER_H_
 
 #include "object2d.h"
 
-
-
 class CNumber : public CObject2d
 {
 public:
-
-	
 	CNumber(int list);
 	~CNumber();
+
 	HRESULT Init() override;
 	void Uninit() override;
 	void Update() override;
 	void Draw() override;
+
 	static CNumber* Create();
 	
 	void SetSpeed(float Speed) { m_Speed = Speed; }
@@ -30,20 +27,16 @@ public:
 	void SetMove(bool Move) { m_Move = Move; }
 	
 	void Releasetimer(int nTimar);
+
 private:
 	D3DXCOLOR m_col;
-
 	float m_Speed;
-
 	int m_DesTimarMax;
 	int m_DesTimar;
-
 	int m_MyNumber;
-
 	bool m_isRelease;
 	bool m_Move;
 	
 };
-
 
 #endif
