34955ddd09cd96f5e7e5e63105f15cdb1d0cbcc9
Yuda Kaito <211019@st.yoshida-g.ac.jp>
2022-09-30 01:01:58 +0900

    object3dをコメントの追加

--- object3d.h
+++ object3d.h
@@ -2,6 +2,7 @@
 //
 // OBJECT3D
 // Author : 浜田琉雅
+// Author : 唐崎結斗
 //
 //=============================================================================
 #ifndef _OBJECT3D_H_			// このマクロ定義がされてなかったら
@@ -16,7 +17,7 @@
 #include "motion.h"
 
 //-----------------------------------------------------------------------------
-// 3D状のポリゴンクラス
+// モデルクラス
 //-----------------------------------------------------------------------------
 class CObject3d : public CObject
 {
@@ -46,7 +47,7 @@ public: // 列挙型
 		DAMEGE_MAX
 	};
 
-public: 
+public:
 	CObject3d();
 	~CObject3d() override;
 	HRESULT Init() override;
@@ -54,19 +55,18 @@ public:
 	void Update() override;
 	void Draw() override;
 
-	virtual void SetPos(const D3DXVECTOR3 &pos) ;
+	// Setter
+	virtual void SetPos(const D3DXVECTOR3 &pos);
 	virtual void SetMove(const D3DXVECTOR3 &move);
-	virtual const D3DXVECTOR3 *GetPos() const ;
-
 	void Set(const D3DXVECTOR3 &pos, const D3DXVECTOR3 &rot, char *filename);
-
 	void SetRot(D3DXVECTOR3 &Rot);
 	void SetSize(D3DXVECTOR3 &Size);
 	void SetLife(int Life);
 
+	// Getter
+	virtual const D3DXVECTOR3 *GetPos() const;
 	int GetLife() { return m_Life; };
 	const D3DXVECTOR3 *GetRot() const;
-
 
 	void HitLife(int Damage);
 	DAMEGE& GetDamegeData();
@@ -76,7 +76,7 @@ public:
 
 protected:
 	D3DXVECTOR3		m_move;				// ムーブ
-	D3DXVECTOR3		m_pos;
+	D3DXVECTOR3		m_pos;				// 位置
 	D3DXVECTOR3		m_rot;				// 回転	
 	D3DXVECTOR3		m_rotMove;			// 回転ムーブ
 	ANIME			m_motionType;		// モーションタイプ(現在)
@@ -89,36 +89,36 @@ private:
 	LPDIRECT3DTEXTURE9	m_pTexture = NULL;
 	LPDIRECT3DVERTEXBUFFER9	m_pVtxBuff = NULL;
 
-	CMotion			*m_pMotion;					// モーション
+	CMotion			*m_pMotion;			// モーション
 
 	// 位置
-	D3DXVECTOR3		m_posOld;					// 位置過去
-	//D3DXVECTOR3		m_move;						// ムーブ
+	D3DXVECTOR3		m_posOld;			// 位置過去
+	//D3DXVECTOR3		m_move;			// ムーブ
 
-	D3DXVECTOR3		m_modelMin;					// サイズ最小
-	D3DXVECTOR3		m_modelMax;					// サイズ最大
-	D3DXMATRIX		m_mtxWorld;					// マトリックス//ポリゴンの位置や回転行列すべてをつめてるナニカ
+	D3DXVECTOR3		m_modelMin;			// サイズ最小
+	D3DXVECTOR3		m_modelMax;			// サイズ最大
+	D3DXMATRIX		m_mtxWorld;			// マトリックス//ポリゴンの位置や回転行列すべてをつめてるナニカ
 
-	int				m_nMaxModelType;			// モデルのタイプ数
-	int				m_nMaxModelParts;			// 扱うモデルパーツ数
-	int				m_nMaxMotion;				// モーション数
+	int				m_nMaxModelType;	// モデルのタイプ数
+	int				m_nMaxModelParts;	// 扱うモデルパーツ数
+	int				m_nMaxMotion;		// モーション数
 
-	int				m_type;						// タイプ
-	int				m_shadow;					// 影番号
-	int				m_invincible;				// 無敵時間
-	int				m_damagecollar;				// ダメージ点滅
-	float			m_consumption;				// 計算用
+	int				m_type;				// タイプ
+	int				m_shadow;			// 影番号
+	int				m_invincible;		// 無敵時間
+	int				m_damagecollar;		// ダメージ点滅
+	float			m_consumption;		// 計算用
 
-	bool			m_bMotionBlend;				// モーションブレンド
-	bool			m_bMotion;					// モーションを使用状況
+	bool			m_bMotionBlend;		// モーションブレンド
+	bool			m_bMotion;			// モーションを使用状況
 
-	bool			m_isUse;					// 使ってるか使ってないか
-	bool			m_isLoop;					// ループするかしないか
+	bool			m_isUse;			// 使ってるか使ってないか
+	bool			m_isLoop;			// ループするかしないか
 
-	int				m_time;						// タイムの最大数
-	int				m_nparts;					// パーツの最大数
-	int				m_pow;						// ジャンプパワー
-	int				m_nMotion;					// モーション番号
+	int				m_time;				// タイムの最大数
+	int				m_nparts;			// パーツの最大数
+	int				m_pow;				// ジャンプパワー
+	int				m_nMotion;			// モーション番号
 	int				m_Life;
 
 	int				m_Invincible;
